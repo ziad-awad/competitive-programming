cf33ed43cd97af3c80ab9341331f564dc7e07a09
Ziad Awad <56039969+ziad-awad@users.noreply.github.com>
2021-04-28 04:31:38 +0200

    Create basic_data_types.cpp

---
+++ hakerrank/basic_data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int num ;
+    long lnum;
+    char ch;
+    float fl;
+    double d;
+
+    scanf("%d %ld %c %f %lf" , &num , &lnum , &ch , &fl , &d);
+
+    printf("%d\n%ld\n%c\n%f\n%lf" , num , lnum , ch , fl , d);
+
+    return 0;
+}
