f584a279bd7fc6a5953e074f9a6bb5e7da19d060
Ziad Awad <56039969+ziad-awad@users.noreply.github.com>
2021-04-28 04:28:37 +0200

    Create inpout_output.cpp

---
+++ hakerrank/inpout_output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+   int n1 , n2 , n3;
+
+   cin>>n1>>n2>>n3;
+   cout<<n1+n2+n3<<endl;  
+    return 0;
+}
